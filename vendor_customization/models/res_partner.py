3e74ba63a2455d96919a8a81c91de05a9c11044e
Muhammad Rizwan <58412444+rizwanch786@users.noreply.github.com>
2023-03-24 17:12:44 +0500

    vendor customer master

--- vendor_customization/models/res_partner.py
+++ vendor_customization/models/res_partner.py
@@ -1,4 +1,7 @@
-from odoo import models, fields, api
+import datetime
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError, UserError
 
 
 class ResPartner(models.Model):
@@ -7,21 +10,39 @@ class ResPartner(models.Model):
     supplier_rank = fields.Integer(default=0, copy=False)
     customer_rank = fields.Integer(default=0, copy=False)
 
-    @api.onchange('vat')
-    def _onchange_vat(self):
-        print(self.supplier_type)
-        print(self.customer_rank)
-        print(self.supplier_rank)
-
     #     Vendor Custom Fields
     name_arabic = fields.Char('Arabic Name')
-    new_gl = fields.Char('New G/L')
+    vcode_start = fields.Integer('5', default=5)
+    vcode_input = fields.Char(size=4)
+    vendor_code = fields.Char('Vendor Code', compute='_compute_vendor_code')
+
+    @api.depends('vcode_input')
+    def _compute_vendor_code(self):
+        for rec in self:
+            if rec.vcode_input:
+                rec.vendor_code = str(rec.vcode_start) + str(rec.vcode_input)
+            else:
+                rec.vendor_code = f'{str(rec.vcode_start)}XXXX'
+
+    # Customer Fields
+    ccode_start = fields.Integer('3', default=3)
+    ccode_input = fields.Char(size=4)
+    customer_code = fields.Char('Customer Code', compute='_compute_customer_code')
+
+    @api.depends('ccode_input')
+    def _compute_customer_code(self):
+        for rec in self:
+            if rec.ccode_input:
+                rec.customer_code = str(rec.ccode_start) + str(rec.ccode_input)
+            else:
+                rec.customer_code = f'{str(rec.ccode_start)}XXXX'
+
     focus_gl = fields.Char('Focus G/L')
+    new_gl = fields.Char('new G/L')
     loc = fields.Selection([
         ('international', 'International'),
         ('domestic', 'Domestic')
     ], string='Location')
-    Comm_reg_no = fields.Char('Commercial Registration Number')
 
     supplier_type = fields.Selection([
         ('manufacturing', 'Manufacturing'),
@@ -36,7 +57,7 @@ class ResPartner(models.Model):
     primary_contact = fields.Char('Primary Contact')
     company_name = fields.Char('Company Name')
     text_treatment = fields.Char('Text Treatment')
-    cr_no = fields.Char('CR Number')
+
     trn = fields.Char('TRN')
     place_of_supply = fields.Many2one(string="Place of Supply", comodel_name='res.country')
     bsn_leg_info = fields.Char('Business Legal Information')
@@ -54,6 +75,105 @@ class ResPartner(models.Model):
     twitter_acc = fields.Char('Twitter')
 
     # same fields
+    Comm_reg_no = fields.Char('CR Number')
+    cr_no = fields.Char('CR Number')
     cr_expiry_date = fields.Date('CR Expiry Date')
-    iktiva_segment = fields.Char('IKTIVA Segment')
-    iktiva_scoring = fields.Char('IKTIVA Scoring')
+    # extra------------------------------------------------------
+    iktiva_segment = fields.Char('IKTIVA Segment', invisible=1)
+    iktiva_scoring = fields.Char('IKTIVA Scoring', invisible=1)
+    # Extraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    # Phone
+    country_code_phone = fields.Char('Country Code', size=3)
+    phone_number = fields.Char('Phone Number', size=10)
+
+    @api.onchange('country_code_phone', 'phone_number')
+    def _on_phone_change(self):
+        for rec in self:
+            if rec.country_code_phone or rec.phone_number:
+                rec.phone = str(rec.country_code_phone) + str(rec.phone_number)
+                if rec.name == self.env.company.partner_id.name:
+                    self.env.company.country_code_phone = str(rec.country_code_phone)
+                    self.env.company.phone_number = str(rec.phone_number)
+
+    # CR Expiry Msg Display to Account & Purchase users
+
+    def cr_expiry_msg(self):
+        print('clicked cr_expiry')
+        if (
+                self.env.user.has_group('account.group_account_manager')
+                or self.env.user.has_group('account.group_account_user')
+        ):
+            for rec in self.env['res.partner'].search([]):
+                if rec.cr_expiry_date <= datetime.date.today():
+                    return {
+                        'type': 'ir.actions.client',
+                        'tag': 'display_notification',
+                        'params': {
+                            'title': ('CR Date Expired'),
+                            'message': f'CR date has been expired of User\nID: {rec.id}\nName: {rec.name}',
+                            'type': 'danger',  # types: success,warning,danger,info
+                            'sticky': True,  # True/False will display for few seconds if false
+                        },
+                    }
+
+    # new fields
+    is_vat = fields.Boolean('VAT', default=False)
+    vat_certificate_id = fields.Many2many(comodel_name="ir.attachment",
+                                          relation="m2m_ir_vat_certificate_rel",
+                                          column1="m2m_id",
+                                          column2="attachment_id",
+                                          )
+    cr_attachment_id = fields.Many2many(comodel_name="ir.attachment",
+                                        relation="m2m_ir_cr_attachment_rel",
+                                        column1="m2m_id",
+                                        column2="attachment_id",
+                                        )
+    ntc_attachment_id = fields.Many2many(comodel_name="ir.attachment",
+                                         relation="m2m_ir_ntc_attachment_rel",
+                                         column1="m2m_id",
+                                         column2="attachment_id",
+                                         )
+
+    # Approvals
+    state = fields.Selection(selection=[
+        ('draft', 'Draft'),
+        ('confirm', 'Confirm'),
+        ('cancel', 'Cancelled')
+    ], string='Status', required=True, readonly=True, copy=False, tracking=True, default='draft')
+
+    def action_confirm(self):
+        self.write({
+            'state': 'confirm'
+        })
+
+    def action_cancel(self):
+        self.write({
+            'state': 'cancel'
+        })
+
+    def action_draft(self):
+        self.write({
+            'state': 'draft'
+        })
+
+
+class InheritedResCompany(models.Model):
+    _inherit = 'res.company'
+
+    country_code_phone = fields.Char('Country Code', size=3)
+    phone_number = fields.Char('Phone Number', size=10)
+
+    @api.onchange('country_code_phone', 'phone_number')
+    def _on_phone_change(self):
+        for rec in self:
+            if rec.country_code_phone or rec.phone_number:
+                rec.phone = str(rec.country_code_phone) + str(rec.phone_number)
+                rec.partner_id.country_code_phone = str(rec.country_code_phone)
+                rec.partner_id.phone_number = str(rec.phone_number)
+
+    @api.constrains('phone_number')
+    def _verify_phone_number(self):
+        for rec in self:
+            if rec.phone_number and not rec.phone_number.isdigit():
+                raise ValidationError(_("The Phone Number must be a sequence of digits."))
+
