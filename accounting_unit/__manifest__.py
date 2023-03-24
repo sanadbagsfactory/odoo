4e627074f4969dd3fa4a56aafc4e3a3837687b6b
Muhammad Rizwan <58412444+rizwanch786@users.noreply.github.com>
2023-03-24 15:41:25 +0500

    vendor customer master

--- accounting_unit/__manifest__.py
+++ accounting_unit/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "vendor_customization",
+    'name': "accounting_unit",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -14,19 +14,22 @@
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'contacts', 'hr', 'account'],
+    'depends': ['base', 'account', 'sale', 'purchase'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'security/security.xml',
-        'views/res_partner_view.xml',
-        'views/hr_employee_view.xml',
+        'views/inherited_account_move.xml',
+        'views/templates.xml',
+        'views/inherited_res_company.xml',
+        'views/inherit_sale_order.xml',
+        'views/inherit_purchase_order.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
