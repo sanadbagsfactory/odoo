195bd5f1319041822fe9754fa770dbad70298b09
azizwazir <61586101+azizwazir@users.noreply.github.com>
2023-04-06 19:50:24 -0400

    Add files via upload

--- contact_customization/models/models.py
+++ contact_customization/models/models.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class RespartnerInheritModel(models.Model):
@@ -6,6 +9,57 @@ class RespartnerInheritModel(models.Model):
 
     check_ktva = fields.Boolean(default=False)
     check_vat_cr = fields.Boolean(default=False)
+    state = fields.Selection([
+        ('create', 'Create'),
+        ('confirm', 'Confirm'),
+        ('approve', 'Approve'),
+        ('cancel', 'Cancel'),
+    ], default='create')
+
+    def action_create(self):
+        self.state = 'create'
+
+    def action_confirm(self):
+        self.state = 'confirm'
+        if self.state == 'confirm':
+            todos = {
+                'res_id': self.id,
+                'res_model_id': self.env['ir.model'].sudo().search([('model', '=', 'res.partner')]).id,
+                'user_id': self.env.user.search([('login', '=', 'azeem.anwar@sanadbags.com')]).id,
+                'summary': f'New Vendor is confirmed {self.name}',
+                'note': 'New Vendor is confirmed',
+                # 'activity_type_id': 4,
+                'date_deadline': datetime.now(),
+            }
+            self.env['mail.activity'].sudo().create(todos)
+
+    def action_cancel(self):
+        self.state = 'cancel'
+        todos = {
+            'res_id': self.id,
+            'res_model_id': self.env['ir.model'].sudo().search([('model', '=', 'res.partner')]).id,
+            'user_id': self.env.user.search([('login', '=', 'baashar.mohammed@sanadbags.com')]).id,
+            'summary': f'New vendor approval is rejected {self.name}',
+            'note': 'New vendor approval is Rejected',
+            # 'activity_type_id': 4,
+            'date_deadline': datetime.now(),
+        }
+        self.env['mail.activity'].sudo().create(todos)
+
+    def action_approve(self):
+        self.state = 'approve'
+        if self.state == 'approve':
+            todos = {
+                'res_id': self.id,
+                'res_model_id': self.env['ir.model'].sudo().search([('model', '=', 'res.partner')]).id,
+                'user_id': self.env.user.search([('login', '=', 'asif.javaed@sanadbags.com')]).id,
+                'summary': f'New Vendor is Approved {self.name}',
+                'note': 'New Vendor is Approved',
+                # 'activity_type_id': 4,
+                'date_deadline': datetime.now(),
+            }
+
+            self.env['mail.activity'].sudo().create(todos)
 
     @api.onchange('country_id')
     def _onchange_country(self):
@@ -29,3 +83,29 @@ class RespartnerInheritModel(models.Model):
             self.check_vat_cr = True
         else:
             self.check_vat_cr = False
+
+
+    @api.model
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        if self.env.user.has_group('contact_customization.view_security_group'):
+            raise ValidationError('Your are not allowed to create contact !')
+        if res:
+            todos = {
+                'res_id': res.id,
+                'res_model_id': self.env['ir.model'].sudo().search([('model', '=', 'res.partner')]).id,
+                'user_id': self.env.user.search([('login', '=', 'baashar.mohammed@sanadbags.com')]).id,
+                'summary': f'New Vendor is confirmed {res.name}',
+                'note': 'New Vendor is confirmed',
+                'date_deadline': datetime.now(),
+            }
+
+            self.env['mail.activity'].sudo().create(todos)
+        return res
+
+
+    def write(self, vals_list):
+        res = super().write(vals_list)
+        if self.env.user.has_group('contact_customization.view_security_group'):
+            raise ValidationError('Your are not allowed to create contact !')
+        return res
