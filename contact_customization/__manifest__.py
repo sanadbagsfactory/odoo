e7849d084a4359e98fd284b8ccdabdf94443918e
Muhammad Rizwan <58412444+rizwanch786@users.noreply.github.com>
2023-04-08 01:23:13 +0500

    contacts customization

--- contact_customization/__manifest__.py
+++ contact_customization/__manifest__.py
@@ -25,8 +25,11 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'security/view_security_group.xml',
         'views/views.xml',
         'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
     ],
 }
