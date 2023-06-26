98b91dfea2f8d13948dccce90cd8c24198e1b0a6
azizwazir <61586101+azizwazir@users.noreply.github.com>
2023-06-26 09:01:37 -0400

    Add files via upload

--- inventory_material_report/models/models.py
+++ inventory_material_report/models/models.py
@@ -3,3 +3,16 @@ from odoo import api, fields, models
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
+
+
+
+class MaterialReceiving(models.AbstractModel):
+    _name = 'report.inventory_material_report.voucher_temp_id'
+
+
+    def _get_report_values(self, docids, data=None):
+
+        return {
+            'document_type': self.env['department.document.type'].search([('department_type', '=', 'whu'),('document_type', '=', '4')]),
+            'docs': self.env['stock.picking'].browse(docids)
+        }
