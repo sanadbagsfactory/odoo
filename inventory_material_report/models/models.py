910373b63bfb11607126e366145496b8d780dadf
azizwazir <61586101+azizwazir@users.noreply.github.com>
2023-06-26 09:13:05 -0400

    Add files via upload

--- inventory_material_report/models/models.py
+++ inventory_material_report/models/models.py
@@ -12,7 +12,14 @@ class MaterialReceiving(models.AbstractModel):
 
     def _get_report_values(self, docids, data=None):
 
+        doc = self.env['department.document.type'].search([('department_type', '=', 'whu'),('document_type', '=', '4')])
+
         return {
-            'document_type': self.env['department.document.type'].search([('department_type', '=', 'whu'),('document_type', '=', '4')]),
+            'document_type': 'Forms' if doc.document_type == '4' else None,
+            'approved_on': doc.approved_on if doc.approved_on else None,
+            'department_type': 'Warehousing' if doc.department_type == 'whu' else None,
+            'revision': doc.revision if doc.revision else None,
+            'notes': doc.notes if doc.notes else None,
+
             'docs': self.env['stock.picking'].browse(docids)
         }
