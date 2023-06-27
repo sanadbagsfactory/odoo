f689c5eb4e00ef70f662eba918fb062ed56cf9a8
azizwazir <61586101+azizwazir@users.noreply.github.com>
2023-06-27 00:10:24 -0400

    Add files via upload

--- inventory_material_report/models/models.py
+++ inventory_material_report/models/models.py
@@ -6,20 +6,21 @@ class StockPicking(models.Model):
 
 
 
-# class MaterialReceiving(models.AbstractModel):
-#     _name = 'report.inventory_material_report.voucher_temp_id'
-#
-#
-#     def _get_report_values(self, docids, data=None):
-#
-#         doc = self.env['department.document.type'].search([('department_type', '=', 'whu'),('document_type', '=', '4')])
-#
-#         return {
-#             'document_type': 'Forms' if doc.document_type == '4' else None,
-#             'approved_on': doc.approved_on if doc.approved_on else None,
-#             'department_type': 'Warehousing' if doc.department_type == 'whu' else None,
-#             'revision': doc.revision if doc.revision else None,
-#             'notes': doc.notes if doc.notes else None,
-#
-#             'docs': self.env['stock.picking'].browse(docids)
-#         }
+class MaterialReceiving(models.AbstractModel):
+    _name = 'report.inventory_material_report.voucher_temp_id'
+
+
+    def _get_report_values(self, docids, data=None):
+
+        doc = self.env['department.document.type'].search([('department_type', '=', 'whu'),('document_type', '=', '4')])
+
+        return {
+            'document_type': 'Forms' if doc.document_type == '4' else None,
+            'approved_on': doc.approved_on if doc.approved_on else None,
+            'department_type': 'Warehousing' if doc.department_type == 'whu' else None,
+            'doc_no': doc.doc_no if doc.doc_no else None,
+            'revision': doc.revision if doc.revision else None,
+            'notes': doc.notes if doc.notes else None,
+
+            'docs': self.env['stock.picking'].browse(docids)
+        }
